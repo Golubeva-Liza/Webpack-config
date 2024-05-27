ea36f2aacac9080dfe862c69bf99d01702ef9354
Liza Golubeva <spblizza@yandex.ru>
2024-05-27 16:27:16 +0300

    install react

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { test } from "./test";
-
-console.log("hello");
-
-console.log(test(1));
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 
 type Mode = 'production' | 'development'
 
@@ -12,35 +12,35 @@ interface EnvVars {
 
 export default (env: EnvVars) => {
     const config: webpack.Configuration = {
-        mode: env.mode ?? "development",
-        entry: path.resolve(__dirname, "src", "index.ts"),
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         output: {
-            path: path.resolve(__dirname, "build"),
-            filename: "[name].[contenthash].js",
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
             clean: true,
         },
         plugins: [
             new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, "public", "index.html"),
+                template: path.resolve(__dirname, 'public', 'index.html'),
             }),
         ],
         module: {
             rules: [
                 {
                     test: /\.tsx?$/,
-                    use: "ts-loader",
+                    use: 'ts-loader',
                     exclude: /node_modules/,
                 },
             ],
         },
         resolve: {
-            extensions: [".tsx", ".ts", ".js"],
+            extensions: ['.tsx', '.ts', '.js'],
         },
-        devtool: "inline-source-map",
+        devtool: 'inline-source-map',
         devServer: {
             port: env.port ?? 8080,
             open: true,
         },
-    };
-    return config;
-};
+    }
+    return config
+}
